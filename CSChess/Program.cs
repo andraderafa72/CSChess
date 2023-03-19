a4954a2158bddc7c3b489ed62544ab6c5be139a8
Rafael Andrade <devrafael72@gmail.com>
2023-03-19 14:42:36 -0300

    Position

--- CSChess/Program.cs
+++ CSChess/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
 
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(i + 1);
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(" _ ");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
